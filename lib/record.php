1c2d4870177a46afac7d14d6e44560a1ee4a9a4b
qq519043202 <519043202@qq.com>
2016-06-13 21:19:56 +0800

    聊天记录SQL修改。。中文编码。。
    之前写错了。。还有中文json的问题。

--- lib/record.php
+++ lib/record.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Content-type: text/html;charset=utf-8");
 include './sql_init.php';
 
 $data = $_POST['data'];
@@ -7,24 +7,40 @@ $data = $_POST['data'];
 // $data['to'] = 'admin0';
 
 $sql1 ="SELECT * FROM  `msg` 
-		WHERE  `from` =  '".$data['from']."'
-		AND  `to` =  '".$data['to']."';";
+		WHERE  (`from` =  '".$data['from']."'
+		AND  `to` =  '".$data['to']."') OR ( `from` =  '".$data['to']."'
+		AND  `to` =  '".$data['from']."') ;";
 
-$sql2 ="SELECT * FROM  `msg` 
-		WHERE  `from` =  '".$data['to']."'
-		AND  `to` =  '".$data['from']."';";
-
+/*
+    字符串GBK转码为UTF-8，数字转换为数字。
+*/
+function ct2($s){
+    if(is_numeric($s)) {
+        return intval($s);
+    } else {
+        return iconv("GBK","UTF-8",$s);
+    }
+}
+/*
+    批量处理gbk->utf-8
+*/
+function icon_to_utf8($s) {
+  if(is_array($s)) {
+    foreach($s as $key => $val) {
+      $s[$key] = icon_to_utf8($val);
+    }
+  } else {
+      $s = ct2($s);
+  }
+  return $s;
+}
 
 $emp_info = array();
 
 $res1 = mysql_query($sql1);
 while($row = mysql_fetch_assoc($res1)){
-    $emp_info[] = $row;
-}
 
-$res2 = mysql_query($sql2);
-while($row = mysql_fetch_assoc($res2)){
-    $emp_info[] = $row;
+    $emp_info[] = icon_to_utf8($row);
 }
 
 echo json_encode($emp_info);
